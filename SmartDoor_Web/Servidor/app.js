cfc6d8a7b71eaafc71e3d8c02752a494c7d9bf65
Joaquín Bazterrica <joaquin.bazterrica@gmail.com>
2018-09-14 08:44:49 -0300

    Acceso y extración de las cookies

--- SmartDoor_Web/Servidor/app.js
+++ SmartDoor_Web/Servidor/app.js
@@ -1,4 +1,5 @@
-//Server Location: C:\Users\43871824\Documents\4to TIC\Proyecto Final\CloudLock\SmartDoor_Web\Servidor
+//Server Location (Axel): C:\Users\43871824\Documents\4to TIC\Proyecto Final\CloudLock\SmartDoor_Web\Servidor
+//Server Location (Joako): C:\Users\43322826\Documents\GitHub\CloudLock\SmartDoor_Web\Servidor
 
 //Initialize Express (Server)
 var express = require('express');
--- SmartDoor_Web/Servidor/public/scripts/admin.js
+++ SmartDoor_Web/Servidor/public/scripts/admin.js
@@ -1,3 +1,23 @@
 console.log("Corriendo Administrador");
 
-$('.user').text(cookie.value);
+var misCookies = document.cookie;
+
+function leerCookie(nombre) {
+ var lista = document.cookie.split(";");
+     for (i in lista) {
+         var busca = lista[i].search(nombre);
+         if (busca > -1) 
+         	{
+         		micookie=lista[i];
+         	}
+         	else
+         	{
+         		window.location.href = "../index.html";
+         	}
+     }
+ var igual = micookie.indexOf("=");
+ var valor = micookie.substring(igual+1);
+return valor;
+}
+
+document.getElementById("name").innerHTML = leerCookie("username");
