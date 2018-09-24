30e9bbd781d5ae30ebdfb24fd17e4adcd2aa819f
Joaquín Bazterrica <joaquin.bazterrica@gmail.com>
2018-09-24 19:23:43 -0300

    Página Web - Administrador de Cuentas

--- SmartDoor_Web/Servidor/public/scripts/admin.js
+++ SmartDoor_Web/Servidor/public/scripts/admin.js
@@ -1,6 +1,6 @@
 console.log("Corriendo Administrador");
 
-var misCookies = document.cookie;
+/*var misCookies = document.cookie;
 
 function leerCookie(nombre) {
  var lista = document.cookie.split(";");
@@ -19,5 +19,31 @@ function leerCookie(nombre) {
  var valor = micookie.substring(igual+1);
 return valor;
 }
-
 document.getElementById("name").innerHTML = leerCookie("username");
+*/
+var num = 0;
+
+$('#agregar').click(function(){
+    if (num === 0) {
+    document.getElementById("1").style.display = "block";
+    console.log("agregar");
+    num += 1;
+} else if (num === 1) {
+    document.getElementById("2").style.display = "block";
+    console.log("agregar");
+    num += 1;
+} else if (num === 2) {
+    document.getElementById("3").style.display = "block";
+    console.log("agregar");
+    num += 1;
+}
+    
+})
+
+$('#eliminar').click(function(){
+    console.log("eliminar");
+})
+
+$('#editar').click(function(){
+    console.log("editar");
+})
