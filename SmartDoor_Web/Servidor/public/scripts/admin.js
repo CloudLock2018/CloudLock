0df7ab624aa92b3f83bf859af6c1e7c699978d39
Joaquín Bazterrica <joaquin.bazterrica@gmail.com>
2018-09-28 11:02:28 -0300

    Página Web - Administrador de Cuentas (#4)
    Botón de Agregar Sub-Usuario

--- SmartDoor_Web/Servidor/public/scripts/admin.js
+++ SmartDoor_Web/Servidor/public/scripts/admin.js
@@ -1,6 +1,6 @@
 console.log("Corriendo Administrador");
 
-var misCookies = document.cookie;
+/*var misCookies = document.cookie;
 
 function leerCookie(nombre) {
  var lista = document.cookie.split(";");
@@ -20,13 +20,13 @@ function leerCookie(nombre) {
 return valor;
 }
 document.getElementById("name").innerHTML = leerCookie("username");
-
+*/
 var num = 0;
 var agreg = false;
 
 $('#agregar').click(function(){
 	if (agreg === false){
-		var agregarNombre = "<div class='contenedor2'><input id='nombre'><input type='button' value='Entrar' id='btn'></div>";
+		var agregarNombre = "<div id='" + num + "' class='contenedor2'><input type='text' placeholder='Usuario' id='nombre'><input class='button' type='button' value='✖' id='bton'><input class='button' type='button' value='✔' id='btn'></div>";
 		$(".contenedor2").show();
 		$("#nombre").show();
 		$("#btn").show();
@@ -45,12 +45,20 @@ $('#btn').click(function(){
 	}
 	else
 	{
+		num += 1
 		agreg = false;
 	}
 })
 
 $('#eliminar').click(function(){
     console.log("eliminar");
+})
+
+$('#bton').click(function(){
+	console.log(num);
+	console.log('.contenedor2 #' + num);
+    $('#' + num).hide();
+    agreg = false;
 })
 
 $('#editar').click(function(){
