c3e2d7202706890625fed1bff70200f1feb8119b
preiti tasat axel facundo <43871824@comunidadort.edu.ar>
2018-09-29 15:02:42 -0300

    Página Web - Administrador de Cuentas (#7)
    Intente setear una cookie a la variable num pero no lo hace

--- SmartDoor_Web/Servidor/public/scripts/admin.js
+++ SmartDoor_Web/Servidor/public/scripts/admin.js
@@ -1,8 +1,8 @@
 console.log("Corriendo Administrador");
 
-/*var misCookies = document.cookie;
+var misCookies = document.cookie;
 
-function leerCookie(nombre) {
+/*function leerCookie(nombre) {
  var lista = document.cookie.split(";");
      for (i in lista) {
          var busca = lista[i].search(nombre);
@@ -21,9 +21,32 @@ return valor;
 }
 document.getElementById("name").innerHTML = leerCookie("username");
 */
+
+/*function cookieNum(numero) {
+    var lista = document.cookie.split(";");
+     for (i in lista) {
+         var busca = lista[i].search(numero);
+         if (busca > -1) 
+            {
+                micookie = lista[i];
+            }
+        else
+            {
+                micookie = 0;
+            }
+     }
+ var igual = micookie.indexOf("=");
+ var valor = micookie.substring(igual+1);
+return valor;
+}
+*/
+
 agre();
+nuevo();
 borrar();
-var num = 0;
+editar();
+
+var num = /*cookieNum("ultagreg")*/ 0;
 var elegido = 0;
 var agreg = false;
 var clickeado = 0;
@@ -34,16 +57,19 @@ function agre(){
 			$("#0").show();
 			agreg = true;
             clickeado += 1;
+            num += 1;
 		}
 		else {
 			if (agreg === false){
-                num += 1;
-				console.log(num);
-				var agregarNombre = "<div id='" + num + "' class='contenedor2'><input class='member' type='text' placeholder='Usuario' id='" + num + "'><input class='error' type='button' value='✖' id='" + num + "'><input class='buttons' type='button' value='✔' id='" + num + "'></div>";
+                var agregarNombre = "<div id='" + num + "' class='contenedor2'><input class='member' type='text' placeholder='Usuario' id='" + num + "'><input class='error' type='button' value='✖' id='" + num + "'><input class='buttons' type='button' value='✔' id='" + num + "'></div>";
 				$(agregarNombre).appendTo(".subusuarios");
 				$('.contenedor2').show();
 				agreg = true;
-				borrar();
+                num += 1;
+                console.log(num);
+                document.cookie = "ultagreg=" + num + "; expires=Fri, 31 Dec 9999 23:59:59 GMT;"
+				nuevo();
+                borrar();
 			}
 			else
 			{
@@ -53,29 +79,32 @@ function agre(){
 	})
 }
 
-$('.buttons').click(function(){
-    if ($('.member').val().length > 0 && clickeado > 0)
-    {
-        agreg = false;
-        console.log("si");
-    }
-    else
-    {
-        console.log("no");
-        agreg = true;
-    }
-});
+function nuevo(){
+    $('.buttons').click(function(){
+        if ($('.member').val().length > 0 && clickeado > 0)
+        {
+            agreg = false;
+            console.log("si");
+        }
+        else
+        {
+            console.log("no");
+            agreg = true;
+        }
+    });
+}
 
 function borrar(){
 	$('.error').click(function(){
 		elegido = this.id;
 		console.log(elegido);
     	$('#' + elegido).closest("div").remove();
-    		num += 1;
-    		agreg = false;
+        agreg = false;
  	});  
 }
 
-$('#editar').click(function(){
-    console.log("editar");
-})
+function editar(){
+    $('#editar').click(function(){
+        console.log("editar");
+    })
+}
