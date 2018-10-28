44b9751d90a4bbcc93686f11528743b46db44612
RamiroYoffe <yofferamiro@gmail.com>
2018-10-28 18:47:44 -0300

    HolaAxel3

--- app.js
+++ app.js
@@ -649,12 +649,14 @@ client.on('message', function (topic, message) {
 				if (abierto === true) {
 					client.publish(Door, 'D1')
 					console.log("abierto");
+					abierto = false;
+					verificar = false;
 				}
 				else if (abierto === false) {
 					client.publish(Door, 'D2')
 					console.log("no existe esa imei");
+					verificar = false;
 				}
-				abierto = false;
 			}
 		}
 		else {
