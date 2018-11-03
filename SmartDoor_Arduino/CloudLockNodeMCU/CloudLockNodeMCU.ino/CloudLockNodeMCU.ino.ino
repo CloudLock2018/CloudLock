8c626f9f8a1310febf8c630c3900c3ace4b871d7
RamiroYoffe <yofferamiro@gmail.com>
2018-11-03 20:51:41 -0300

    Works

--- SmartDoor_Arduino/CloudLockNodeMCU/CloudLockNodeMCU.ino/CloudLockNodeMCU.ino.ino
+++ SmartDoor_Arduino/CloudLockNodeMCU/CloudLockNodeMCU.ino/CloudLockNodeMCU.ino.ino
@@ -75,6 +75,7 @@ void setup() {
   pinMode(ledR, OUTPUT);
   pinMode(ledB, OUTPUT);
   pinMode(ledG, OUTPUT);
+  pinMode(A0, INPUT);
   
   // Connect to WiFi access point.
   Serial.println();
@@ -106,6 +107,7 @@ void setup() {
 }
 
 void loop() {
+  checkRstButton();
   MQTT_connect();
   Serial.println("Reseting");
 
@@ -234,6 +236,17 @@ void statusChecking() {
   Serial.println("The next IMEI will be checked to see if it is registered");
 }
 
+void checkRstButton(){
+   if (analogRead(A0) == 0) {
+     WiFi.disconnect();
+     if (!wifiManager.startConfigPortal("CloudLock", "cloudlock")) {
+        delay(3000);
+        ESP.reset();
+        delay(5000);
+     }
+   }
+}
+
 //Connects and reconnects as necessary to the MQTT server.
 void MQTT_connect() {
   int8_t ret;
@@ -245,18 +258,12 @@ void MQTT_connect() {
   }
 
   Serial.print("Connecting to MQTT... ");
-
-  uint8_t retries = 3;
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    checkRstButton();
     Serial.println(mqtt.connectErrorString(ret));
     Serial.println("Retrying MQTT connection in 5 seconds...");
     mqtt.disconnect();
     delay(5000);  // wait 5 seconds
-    retries--;
-    if (retries == 0) {
-      // basically die and wait for WDT to reset me
-      while (1);
-    }
   }
   Serial.println("MQTT Connected!");
 }
